48fa2f1879f97671a5f8369054a22f4380f652ec
xuedong <shang.xuedong@yahoo.fr>
2017-12-30 23:26:14 +0100

    Problem 4.

---
+++ problem_4/largest_palindrome_product.ml
@@ -0,0 +1,19 @@
+let is_palindrome n =
+  let s = string_of_int n in
+  let l = String.length s in
+  let rec aux n =
+    (n = 0) || (s.[l-n] = s.[n-1] && aux (n-1)) in
+  aux (l/2)
+
+let largest_product limit =
+  let current_max = ref 0 in
+  for i = 1 to limit do
+    for j = i to limit do
+      let prod = i*j in
+      if (is_palindrome prod) && (prod > !current_max) then
+        current_max := prod
+    done
+  done;
+  !current_max;;      
+
+largest_product 999;;
