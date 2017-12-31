c8d778934dc867771375aebab46658258b868185
xuedong <shang.xuedong@yahoo.fr>
2017-12-31 01:36:33 +0100

    Problem 1.
    New constant complexity solution.

--- problem_1/multiples_of_3_and_5.ml
+++ problem_1/multiples_of_3_and_5.ml
@@ -1,9 +1,17 @@
-let sum_multiples n =
+(* Linear complexity *)
+let sum_multiples_1 n =
   let sum = ref 0 in
   for i = 3 to (n-1) do
     if (i mod 3) == 0 || (i mod 5) == 0 then
       sum := !sum + i;
   done;
-  !sum;;
+  !sum
 
-sum_multiples 1000;;
+(* Constant complexity *)
+let sum_multiples_2 n =
+    let sum3 = ((n-1)/3) * (3 * ((n-1)/3) + 3) / 2 in
+    let sum5 = ((n-1)/5) * (5 * ((n-1)/5) + 5) / 2 in
+    let sum15 = ((n-1)/15) * (15 * ((n-1)/15) + 15) / 2 in
+    sum3 + sum5 - sum15;;
+
+sum_multiples_2 1000;;
